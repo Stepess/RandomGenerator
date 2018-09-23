c0682df0fc8c26928de71a1836bdda3e171f1e74
Stepan Yershov <stepanersh@gmail.com>
2018-09-23 23:14:01 +0300

    add geffe generator

--- src/main/java/ua/asymetric/cryptology/App.java
+++ src/main/java/ua/asymetric/cryptology/App.java
@@ -1,9 +1,6 @@
 package ua.asymetric.cryptology;
 
-import ua.asymetric.cryptology.random.L20Generator;
-import ua.asymetric.cryptology.random.LahmerHighGenerator;
-import ua.asymetric.cryptology.random.LahmerLowGenerator;
-import ua.asymetric.cryptology.random.RandomGenerator;
+import ua.asymetric.cryptology.random.*;
 
 import java.util.Arrays;
 
@@ -13,21 +10,18 @@ public class App
     {
         RandomGenerator generator = new LahmerLowGenerator();
         System.out.println(Integer.toBinaryString(generator.generate()));
+
         RandomGenerator generator1 = new LahmerHighGenerator();
         System.out.println(Integer.toBinaryString(generator1.generate()));
+
         RandomGenerator generator2 = new L20Generator();
         System.out.println(generator2.generate());
 
-        StringBuffer sb = new StringBuffer();
-        for (int i=0; i<89; i++){
-            if (i==0 || i == 51) {
-                sb.append("1, ");
-            } else {
-                sb.append("0, ");
-            }
-        }
+        RandomGenerator generator3 = new L89Generator();
+        System.out.println(generator3.generate());
 
-        System.out.println(sb.toString());
+        RandomGenerator generator4 = new GeffeGenerator();
+        System.out.println(generator4.generate());
 
     }
 }
---
+++ src/main/java/ua/asymetric/cryptology/random/GeffeGenerator.java
@@ -0,0 +1,38 @@
+package ua.asymetric.cryptology.random;
+
+import ua.asymetric.cryptology.LSR;
+
+public class GeffeGenerator implements RandomGenerator {
+
+    private LSR L11;
+    private LSR L9;
+    private LSR L10;
+
+    private final static int PERIOD_L11 = 11;
+    private final static int PERIOD_L9 = 9;
+    private final static int PERIOD_L10 = 10;
+
+    private final static int[] RECURRENCE_L11 = {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0};
+    private final static int[] RECURRENCE_L9 = {1, 1, 0, 1, 1, 0, 0, 0, 0};
+    private final static int[] RECURRENCE_L10 = {1, 0, 0, 1, 0, 0, 0, 0, 0, 0};
+
+    public GeffeGenerator() {
+        L11 = new LSR(PERIOD_L11, RECURRENCE_L11);
+        L9 = new LSR(PERIOD_L9, RECURRENCE_L9);
+        L10 = new LSR(PERIOD_L10, RECURRENCE_L10);
+        updateState();
+    }
+
+    @Override
+    public void updateState() {
+        L11.setCondition(System.nanoTime());
+        L9.setCondition(System.nanoTime());
+        L10.setCondition(System.nanoTime());
+    }
+
+    @Override
+    public int generate() {
+        int flag = L10.tact();
+        return (flag & L11.tact()) ^ (1 ^ flag) & L9.tact();
+    }
+}
